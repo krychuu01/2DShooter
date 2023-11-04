4840dd657535ce8e18b3cf1fa6e78bc2fd990444
krychuu01 <hazzidy125@gmail.com>
2023-11-04 22:13:03 +0100

    improved enemy movement - so its always moving and changing direction randomly

--- Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -13,10 +13,12 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody2D rigidbody2D;
     private PlayerAwarnessController playerAwarnessController;
     private Vector2 targetDirection;
+    private float changeDirectionCooldown;
 
     private void Awake() {
         rigidbody2D = GetComponent<Rigidbody2D>();
         playerAwarnessController = GetComponent<PlayerAwarnessController>();
+        targetDirection = transform.up;
     }
 
     // Update is called once per frame
@@ -28,18 +30,12 @@ public class EnemyMovement : MonoBehaviour
     }
 
     private void UpdateTargetDirection() {
-        if (playerAwarnessController.AwareOfPlayer) {
-            targetDirection = playerAwarnessController.DirectionToPlayer;
-        }
-        else {
-            targetDirection = Vector2.zero;
-        }
+        HandleRandomDirectionChange();
+        HandlePlayerTargeting();
+        
     }
 
     private void RotateTowardsTarget() {
-        if (targetDirection == Vector2.zero) {
-            return;
-        }
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
         Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
@@ -47,12 +43,24 @@ public class EnemyMovement : MonoBehaviour
     }
 
     private void SetVelocity() {
-        if (targetDirection == Vector2.zero) {
-            rigidbody2D.velocity = Vector2.zero;
-        }
-        else {
-            rigidbody2D.velocity = transform.up * speed;
+        rigidbody2D.velocity = transform.up * speed;
+    }
+
+    private void HandleRandomDirectionChange() {
+        changeDirectionCooldown -= Time.deltaTime;
+
+        if (changeDirectionCooldown <= 0) {
+            float randomAngle = Random.Range(-90f, 90f); // setting random angle from -90 to 90 degrees
+            Quaternion rotation = Quaternion.AngleAxis(randomAngle, transform.forward); // setting rotation so enemy will turn to a random angle, forward
+            targetDirection = rotation * targetDirection; // multiplying existing targetDirection with 'rotation' value to turn enemy in a specific way
+
+            changeDirectionCooldown = Random.Range(1f, 5f); // setting cooldown from 1 to 5 seconds
         }
     }
 
+    private void HandlePlayerTargeting() {
+        if (playerAwarnessController.AwareOfPlayer) {
+            targetDirection = playerAwarnessController.DirectionToPlayer;
+        }
+    }
 }
